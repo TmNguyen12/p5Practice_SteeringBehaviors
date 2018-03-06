610f081360fc7f01a3adbea5413726fc93895bf1
Truong Nguyen <tmnguyen12@gmail.com>
2018-03-05 22:05:37 -0800

    Ping pong balls

--- sketch.js
+++ sketch.js
@@ -5,12 +5,23 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(800,300); 
+  createCanvas(1000,300); 
   background(51); 
-  textFont(font);
-  textSize(128); 
-  fill(255); 
-  text('rainbow', 10, 200); 
+  // textFont(font);
+  // textSize(192); 
+  // fill(255); 
+  // noStroke(); 
+  // text('rainbow', 30, 200); 
+
+  var points = font.textToPoints('Whimsical', 30, 200, 192); 
+  console.log(points); 
+
+  for (let i = 0; i < points.length; i++){
+    let pt = points[i]; 
+    stroke(255);
+    strokeWeight(6);
+    point(pt.x, pt.y); 
+  }
 }
 
 function draw() {
